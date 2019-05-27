1d06f9007854d72e6266498fea4f8672ae06efd0
Jackz <me@jackz.me>
2019-05-27 19:39:02 +0000

    fixed EntityDamage

--- src/main/java/me/jackz/lobbytools/EntityDamage.java
+++ src/main/java/me/jackz/lobbytools/EntityDamage.java
@@ -8,20 +8,21 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 public class EntityDamage implements Listener {
-    final Main plugin;
-    public EntityDamage(Main plugin) {this.plugin = plugin;}
+    private final Main plugin;
+    EntityDamage(Main plugin) {this.plugin = plugin;}
 
     @EventHandler
     public void onEntityDamage(EntityDamageEvent e) {
-        if(e.getEntityType() == EntityType.PLAYER) {
-            if(e.getCause() == EntityDamageEvent.DamageCause.FALL) {
+        if (e.getEntityType() == EntityType.PLAYER) {
+            if (e.getCause() == EntityDamageEvent.DamageCause.FALL) {
                 String config_world = plugin.getConfig().getString("lobby_world");
-                if(config_world != null) {
+                if (config_world != null) {
                     World world = Bukkit.getWorld(config_world);
-                    if(e.getEntity().getWorld().equals(world)) {
+                    if (e.getEntity().getWorld().equals(world)) {
                         e.setCancelled(true);
                     }
                 }
             }
         }
+    }
 }
