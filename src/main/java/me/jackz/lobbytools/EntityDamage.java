c124e5c0d782fbb67c6af94f1f41b8240001d4a7
Jackz <me@jackz.me>
2019-05-30 19:27:12 -0500

    set players food, saturation, and health on damage
    Signed-off-by: Jackz <me@jackz.me>

--- src/main/java/me/jackz/lobbytools/EntityDamage.java
+++ src/main/java/me/jackz/lobbytools/EntityDamage.java
@@ -1,6 +1,8 @@
 package me.jackz.lobbytools;
 
+import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
@@ -12,11 +14,15 @@ class EntityDamage implements Listener {
     @EventHandler
     public void onEntityDamage(EntityDamageEvent e) {
         if (e.getEntityType() == EntityType.PLAYER) {
+            Player p = (Player) e.getEntity();
             if (e.getCause() == EntityDamageEvent.DamageCause.FALL) {
                 if (e.getEntity().getWorld().equals(Main.world)) {
                     e.setCancelled(true);
                 }
             }
+            p.setHealth(p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue());
+            p.setSaturation(20);
+            p.setFoodLevel(20);
         }
     }
 }
